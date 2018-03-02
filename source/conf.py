acf36c36925c60a21a2fc2a7f1a62c96506f873b
Christian Scholten <scholchr@gmail.com>
2018-03-02 13:59:04 +0100

    Copyright entfernt, logo_only false

--- source/conf.py
+++ source/conf.py
@@ -94,7 +94,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'logo_only': True
+    'logo_only': False
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -161,7 +161,7 @@ html_sidebars = {
 html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
